c13ed27c43ebe840b0c9adba8b58f82fb765d598
i-roger <mroger.dev@gmail.com>
2026-06-21 17:11:55 -0300

    Update: Full upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -6,6 +7,11 @@ const jetbrainsMono = JetBrains_Mono({
   weight: ['100', '300', '400', '700', '800'],
   variable: '--font-mono',
 });
+
+export const metadata: Metadata = {
+  title: 'Rogertech',
+  description: 'RogerTech é uma empresa de engenharia de software especializada em landing pages de alta conversão, automações para WhatsApp, aplicações web modernas e soluções digitais focadas em performance, SEO e aumento de faturamento para empresas, além de contar com um setor especializado em manutenção, formatação e otimização de computadores e notebooks.'
+}
 
 export default function RootLayout({
   children,
